0ac9aa1def4c687702461775bbb49774f4512220
Abdelrahman Hawwari (1000011934) <HAWWAG2@student.op.ac.nz>
2017-10-28 17:18:24 +1300

    getting modal to work with image slider

--- multimedia/assets/js/modalfunction.js
+++ multimedia/assets/js/modalfunction.js
@@ -1,16 +1,16 @@
-https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal_img
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal_img
+//Inspired by Samantha Watson
+
 var modal = document.getElementById("myModal");
 var img1 = document.getElementById("expenseIQ");
 var img2 = document.getElementById("wanakabackground");
-var img3 = document.getElementById("expenseIQ");
-var img4 = document.getElementById("expenseIQ");
 var modalImg = document.getElementById("img01");
 // i is a function parameter that is passed in to an anonymous function
 //like in java data types are declared. In Javascript they are not.
 function img_click(i){
   i.onclick=function(){
     modal.style.display="block";
-    modal_img.src=this.src;
+    modalImg.src=this.src;
     }
 }
 // Get the <span> element that closes the modal
