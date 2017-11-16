ee1606c9d41ac275f87c29ec5174d077dbc4be9e
Abdelrahman Hawwari (1000011934) <HAWWAG2@student.op.ac.nz>
2017-11-16 18:53:37 +1300

    i am committing

--- multimedia/assets/js/modalfunctionlifehack.js
+++ multimedia/assets/js/modalfunctionlifehack.js
@@ -1,10 +1,13 @@
 //https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal_img
 //Inspired by Samantha Watson
 
-var modal = document.getElementById("mysoundscapeModal");
-var img1 = document.getElementById("usingAudacity");
-var img2 = document.getElementById("changetempos");
-var img3 = document.getElementById("soundscapeplanning");
+var modal = document.getElementById("mylifehackmodal");
+var img1 = document.getElementById("lifehackvideosketch");
+var img2 = document.getElementById("usingcaptionoverlay");
+var img3 = document.getElementById("usingkeyframes");
+var img4 = document.getElementById("colourgrading");
+var img5 = document.getElementById("usingtacksandcolo");
+var img6 = document.getElementById("colourtracking");
 
 
 var modalImg = document.getElementById("img01");
@@ -25,4 +28,7 @@ span.onclick=function(){
 img_click(img1);
 img_click(img2);
 img_click(img3);
+img_click(img4);
+img_click(img5);
+img_click(img6);
 
